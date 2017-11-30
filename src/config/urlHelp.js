130ee29c799b40c0dece4d103bf75379819fabf1
zyyyy <410419243@qq.com>
2017-11-30 16:27:58 +0800

    增加接口地址

--- src/config/urlHelp.js
+++ src/config/urlHelp.js
@@ -1,18 +1,19 @@
-define([], function() {
-    var urlHelp = {
-      'isReal':false,
-      'real':{
+define([], function () {
+  var urlHelp = {
+    isReal: false,
+    real: {
 
-      },
-      'simulation':{
-        tableconfig: './data/tableconfig.json',
-        tableDatas: './data/tableDatas.json',
-        newTabDatas: './data/newTabDatas.json',
-        builder_search: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPType=Search',
-        getConfig: './data/getConfig.json',
-        search: './data/search.json',
-        calendar_demo: './data/calendar_demo.json',
-      }
+    },
+    simulation: {
+      tableconfig: './data/tableconfig.json',
+      tableDatas: './data/tableDatas.json',
+      newTabDatas: './data/newTabDatas.json',
+      builder_search: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPType=Search',
+      getConfig: './data/getConfig.json',
+      search: './data/search.json',
+      calendar_demo: './data/calendar_demo.json',
+      secRecordPath: './data/s_problem_record_hy.json'
     }
-    return urlHelp
+  }
+  return urlHelp
 });
