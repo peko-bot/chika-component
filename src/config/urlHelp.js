c6b7ce6dd66eb39480ea459447343b1825ee3104
zyyyy <410419243@qq.com>
2017-12-05 11:40:50 +0800

    Add files via upload

--- src/config/urlHelp.js
+++ src/config/urlHelp.js
@@ -2,15 +2,17 @@ define([], function () {
   var urlHelp = {
     isReal: false,
     real: {
-
+      search: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPType=Search',
+      getConfig: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPtype=GetConfig&UseType=DataList&DataType=1,3',
+      detail: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPtype=GetConfig&DataType=1,3,4',
     },
     simulation: {
       tableconfig: './data/tableconfig.json',
       tableDatas: './data/tableDatas.json',
       newTabDatas: './data/newTabDatas.json',
       builder_search: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPType=Search',
-      getConfig: './data/getConfig.json',
-      search: './data/search.json',
+      getConfig: './data/getConfig_new.json',
+      search: './data/search_new.json',
       calendar_demo: './data/calendar_demo.json',
       secRecordPath: './data/s_problem_record_hy.json'
     }
