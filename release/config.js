99fbc491d939e0da5d37a9a10896a52faa1e21ac
zyyyy <410419243@qq.com>
2017-12-22 16:34:51 +0800

    增加打包后配置

--- release/config.js
+++ release/config.js
@@ -1,62 +1,74 @@
-(function () {
-    var MOUNT_NODE = document.getElementById('root')
-    T.require(['./config/urlHelp'], function (urlHelp) {
-        window.urlHelp  = urlHelp;
-        var inject = function (conf) {
-            var basePath = (urlHelp.isReal)
-                ? urlHelp.real[conf.key]
-                : urlHelp.simulation[conf.key];
-            var theRequest = new Object();
-            if (basePath.indexOf("?") !== -1) {
-                var str = basePath.substr(basePath.indexOf("?") + 1);
-                var strs = str.split("&");
-                if(conf.method != 'GET'){
-                    var param = '';
-                    for(var i = 0;i < strs.length;i++){
-                        var key = strs[i].split('=')[0];
-                        var value = strs[i].split('=')[1];
-                        if(key == 'key'){
-                            continue;
-                        }
-                        theRequest[key] = value;
+var urlHelp = {
+    'isReal': true,
+    'real':{
+		search: '../../webapi/api/v2/generalbackstage/getdata',
+		getConfig: '../../webapi/api/v2/generalbackstage/getconfig',
+		getUrlData: './data/tableconfig.json', // 第三方接口地址，为了放置跨域问题，后端先处理下
+		generalbackstage: '../../webapi/api/v2/generalbackstage/operatedata', // 增删改
+    },
+	'simulation': {}
+}
+
+
+//try{
+T(function () {
+    window.urlHelp  = urlHelp;
+    var inject = function (conf) {
+        var basePath = (urlHelp.isReal)
+            ? urlHelp.real[conf.key]
+            : urlHelp.simulation[conf.key];
+        var theRequest = new Object();
+        if (basePath.indexOf("?") !== -1) {
+            var str = basePath.substr(basePath.indexOf("?") + 1);
+            var strs = str.split("&");
+            
+            var param = '';
+            if(conf.method != 'GET'){
+                for(var i = 0;i < strs.length;i++){
+                    var key = strs[i].split('=')[0];
+                    var value = strs[i].split('=')[1];
+                    if(key == 'key'){
+                        continue;
                     }
-                    theRequest = (urlHelp.isReal)
-                        ? Object.assign({}, theRequest, conf.data)
-                        : Object.assign({}, conf.data, theRequest);
-                    conf.data = theRequest;
-                }else{
-                    var param = '';
-                    basePath = basePath.substring(0,basePath.indexOf("?"));
-                    for(var key in conf.data){
-                        strs.push(key + '=' + conf.data[key]);
-                    }
-                    for(var i = 0; i < strs.length; i++){
-                        var key = strs[i].split('=')[0];
-                        var value = strs[i].split('=')[1];
-                        if(i == 0){
-                            param += '?' + key + '=' + value;
-                        }else{
-                            param += '&' + key + '=' + value;
-                        }
-                    }
-                    basePath += param;
-                    conf.data = null;
+                    theRequest[key] = value;
                 }
+                theRequest = (urlHelp.isReal)
+                    ? Object.assign({}, theRequest, conf.data)
+                    : Object.assign({}, conf.data, theRequest);
+                conf.data = theRequest;
+            }else{
+                basePath = basePath.substring(0,basePath.indexOf("?"));
+                for(var key in conf.data){
+                    strs.push(key + '=' + conf.data[key]);
+                }
+                for(var i = 0; i < strs.length; i++){
+                    var key = strs[i].split('=')[0];
+                    var value = strs[i].split('=')[1];
+                    if(i == 0){
+                        param += '?' + key + '=' + value;
+                    }else{
+                        param += '&' + key + '=' + value;
+                    }
+                }
+                basePath += param;
+                conf.data = null;
             }
-            conf.url = basePath;
         }
+        theRequest = (urlHelp.isReal)
+            ? Object.assign({}, theRequest, conf.data)
+            : Object.assign({}, conf.data, theRequest);
+        conf.data = theRequest;
+        conf.url = basePath;
+    }
+
+    try{
         T.config({
-            mount: MOUNT_NODE, //挂载
             data: {
                 leaf: {
                     deps: [
                         'react', 'react-dom'
                     ],
                     paths: {
-                        Swiper_demo: {
-                            mount: 'Swiper_demo',
-                            deps: ['Swiper_demo', 'css!./style/Swiper_demo']
-                        },
                         List_Container_demo: {
                             mount: 'List_Container_demo',
                             deps: ['List_Container_demo', 'css!./style/List_Container_demo']
@@ -68,11 +80,7 @@
                 go: '/List_Container_demo', //默认路由（当剝路由为空时跳转）
                 route: {
                     path: '/',
-                    once: onTestLayout,
                     children: [
-                        {
-                            path: '/Swiper_demo', leaf: ['Swiper_demo']
-                        },
                         {
                             path: '/List_Container_demo', leaf: ['List_Container_demo']
                         }
@@ -80,27 +88,29 @@
                 }
             },
             ajax: {
-                inject: inject
+                inject: inject,
+                f: 'json'
             },
             req: {
-                baseUrl: './',
+            // baseUrl: './',
+                baseUrl: T.Sys.jsURL(),//'tree',
                 packages: [],
                 map: {
                     '*': {
                         'css': 'plugins/css.min'
                     }
                 },
+				waitSeconds: 0,
                 paths: {
-                    "Swiper_demo": 'modules/Swiper_demo',
                     'List_Container_demo': 'modules/List_Container_demo',
                     "react": 'plugins/react.min',
                     "react-dom": 'plugins/react-dom.min'
                 },
-                shim: {}
             }
-
         })
-    })
+    }catch(e){
+       console.log(e)
+    }
 
-    function onTestLayout(data) {}
-})()
+})
+//}catch(e){alert(e.message)}
--- release/config/urlHelp.js
+++ release/config/urlHelp.js
@@ -1,18 +1,24 @@
-define([], function() {
-    var urlHelp = {
-      'isReal':false,
-      'real':{
-
-      },
-      'simulation':{
-        "secRecordPath": './data/s_problem_record_hy.json',
-        tableconfig: './data/tableconfig.json',
-        tableDatas: './data/tableDatas.json',
-        newTabDatas: './data/newTabDatas.json',
-        builder_search: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPType=Search',
-        getConfig: './data/getConfig.json',
-        search: './data/search.json',
-      }
+define([], function () {
+  var urlHelp = {
+    isReal: true,
+    real: {
+      search: 'http://47.95.1.229:8500/webapi/api/v2/generalbackstage/getdata',
+      getConfig: 'http://47.95.1.229:8500/webapi/api/v2/generalbackstage/getconfig',
+      getUrlData: './data/tableconfig.json', // 第三方接口地址，为了放置跨域问题，后端先处理下
+      generalbackstage: 'http://47.95.1.229:8500/webapi/api/v2/generalbackstage/operatedata', // 增删改
+    },
+    simulation: {
+      tableconfig: './data/tableconfig.json',
+      tableDatas: './data/tableDatas.json',
+      newTabDatas: './data/newTabDatas.json',
+      builder_search: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPType=Search',
+      getConfig: './data/getConfig_new.json',
+      search: './data/search_new.json',
+      calendar_demo: './data/calendar_demo.json',
+      secRecordPath: './data/s_problem_record_hy.json',
+      getUrlData: './data/tableconfig.json', // 第三方接口地址，为了放置跨域问题，后端先处理下
+      generalbackstage: './data/tableconfig.json', // 增删改
     }
-    return urlHelp
+  }
+  return urlHelp
 });
