12e980a6a815b6986060628140b31e18811abad3
zyyyy <410419243@qq.com>
2017-12-22 16:31:39 +0800

    Add files via upload

--- src/config/urlHelp.js
+++ src/config/urlHelp.js
@@ -1,10 +1,11 @@
 define([], function () {
   var urlHelp = {
-    isReal: false,
+    isReal: true,
     real: {
-      search: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPType=Search',
-      getConfig: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPtype=GetConfig&UseType=DataList&DataType=1,3',
-      detail: '../../CommonReport/AjaxHandler/BuilderHandler.ashx?OPtype=GetConfig&DataType=1,3,4',
+      search: 'http://47.95.1.229:8500/webapi/api/v2/generalbackstage/getdata',
+      getConfig: 'http://47.95.1.229:8500/webapi/api/v2/generalbackstage/getconfig',
+      getUrlData: './data/tableconfig.json', // 第三方接口地址，为了放置跨域问题，后端先处理下
+      generalbackstage: 'http://47.95.1.229:8500/webapi/api/v2/generalbackstage/operatedata', // 增删改
     },
     simulation: {
       tableconfig: './data/tableconfig.json',
@@ -14,7 +15,9 @@ define([], function () {
       getConfig: './data/getConfig_new.json',
       search: './data/search_new.json',
       calendar_demo: './data/calendar_demo.json',
-      secRecordPath: './data/s_problem_record_hy.json'
+      secRecordPath: './data/s_problem_record_hy.json',
+      getUrlData: './data/tableconfig.json', // 第三方接口地址，为了放置跨域问题，后端先处理下
+      generalbackstage: './data/tableconfig.json', // 增删改
     }
   }
   return urlHelp
