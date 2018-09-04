ee45886c6fa92c88e89fc8ad6b3908f40ace69b3
zy410419243 <zy410419243@gmail.com>
2018-09-04 14:43:39 +0800

    add upload plus and its click event

--- src/component/Upload/index.js
+++ src/component/Upload/index.js
@@ -2,9 +2,11 @@
  * @Author: zy9@github.com/zy410419243
  * @Date: 2018-09-04 13:44:01
  * @Last Modified by: zy9
- * @Last Modified time: 2018-09-04 13:46:23
+ * @Last Modified time: 2018-09-04 14:41:07
  */
 import React, { Component } from 'react';
+
+import './css/index.css';
 
 export default class Upload extends Component {
 	constructor (props) {
@@ -19,9 +21,31 @@ export default class Upload extends Component {
 
     }
 
+    wrapperOnClick = () => {
+    	let el = this.uploadInput;
+
+    	if (!el) {
+    		return;
+    	}
+
+    	el.click();
+    	el.value = '';
+    }
+
     render = () => {
     	return (
-    		<div className='Upload'>Upload</div>
+    		<div className='Upload'>
+    			<div onClick={ this.wrapperOnClick } className='wrapper'>
+    				<span className='upload-button'>
+    			        <input type='file' ref={ ref => this.uploadInput = ref } style={{ display: 'none' }} />
+
+    					<div>
+    						<i className='plus-icon'>+</i>
+    						<div>upload</div>
+    					</div>
+    				</span>
+    			</div>
+    		</div>
     	);
     }
 }
