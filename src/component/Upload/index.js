f4f4ff2a155c8e5d7b55241af40adaa016a54471
zy410419243 <zy410419243@gmail.com>
2018-09-04 17:28:10 +0800

    add upload logic

--- src/component/Upload/index.js
+++ src/component/Upload/index.js
@@ -2,7 +2,7 @@
  * @Author: zy9@github.com/zy410419243
  * @Date: 2018-09-04 13:44:01
  * @Last Modified by: zy9
- * @Last Modified time: 2018-09-04 14:41:07
+ * @Last Modified time: 2018-09-04 16:28:20
  */
 import React, { Component } from 'react';
 
@@ -17,10 +17,6 @@ export default class Upload extends Component {
 		};
 	}
 
-    componentDidMount = () => {
-
-    }
-
     wrapperOnClick = () => {
     	let el = this.uploadInput;
 
@@ -32,12 +28,35 @@ export default class Upload extends Component {
     	el.value = '';
     }
 
+    onChange = e => {
+    	const { multiple, onStart } = this.props;
+    	let files = e.target.files;
+
+    	if (files.length > 0) {
+    		for (let file of files) {
+    			this.upload(file);
+    		}
+
+    		// if (multiple) {
+    		// 	onStart(Array.prototype.slice.call(files));
+    		// } else {
+    		// 	onStart(Array.prototype.slice.call(files)[0]);
+    		// }
+    	}
+    }
+
+    upload = file => {
+    	const { data, onChange } = this.props;
+
+    	onChange && onChange(file);
+    }
+
     render = () => {
     	return (
     		<div className='Upload'>
     			<div onClick={ this.wrapperOnClick } className='wrapper'>
     				<span className='upload-button'>
-    			        <input type='file' ref={ ref => this.uploadInput = ref } style={{ display: 'none' }} />
+    					<input type='file' ref={ ref => this.uploadInput = ref } style={{ display: 'none' }} onChange={ this.onChange } />
 
     					<div>
     						<i className='plus-icon'>+</i>
--- src/modules/Upload_demo/index.js
+++ src/modules/Upload_demo/index.js
@@ -2,7 +2,7 @@
  * @Author: zy9@github.com/zy410419243
  * @Date: 2018-09-04 13:46:42
  * @Last Modified by: zy9
- * @Last Modified time: 2018-09-04 13:47:19
+ * @Last Modified time: 2018-09-04 16:27:36
  */
 import React, { Component } from 'react';
 
@@ -21,10 +21,14 @@ export default class index extends Component {
 
     }
 
+	onChange = file => {
+		console.log(file);
+	}
+
     render = () => {
     	return (
     		<div className='index'>
-    			<Upload />
+    			<Upload fileList={ [] } onChange={ this.onChange } />
     		</div>
     	);
     }
