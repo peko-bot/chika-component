eeb0c55eb332bcb5a0c8952fc8ca7b894a06ccf2
zy410419243 <zy410419243@gmail.com>
2018-09-14 15:09:25 +0800

    add basic transform of drawer

--- src/component/Drawer/index.js
+++ src/component/Drawer/index.js
@@ -2,7 +2,7 @@
  * @Author: zy9@github.com/zy410419243
  * @Date: 2018-09-14 13:54:25
  * @Last Modified by: zy9
- * @Last Modified time: 2018-09-14 13:54:59
+ * @Last Modified time: 2018-09-14 15:08:52
  */
 import React, { Component } from 'react';
 
@@ -13,17 +13,31 @@ export default class Drawer extends Component {
 		super(props);
 
 		this.state = {
-
+			position: 0,
+			iconDeg: 90,
 		};
 	}
 
-    componentDidMount = () => {
-
-    }
-
     render = () => {
+    	const { children, width = 400, operaNode, direction = 'left', visible, isOperateShow = true, clientView } = this.props;
+    	const { position, iconDeg } = this.state;
+
+    	const operateDrawer = (
+    		<div className='operate-drawer' style={{ transform: `translateX(${ position + width }px)` }} onClick={ () => this.setState({ position: position == 0 ? -width : 0, iconDeg: position == 0 ? 180 : 0 }) }>
+    			<div className='scale-wrapper' style={{ transform: `rotateZ(${ iconDeg }deg)` }}>
+    				{ operaNode ? operaNode : <img src='../../assets/Drawer/arrow.png' /> }
+    			</div>
+    		</div>
+    	);
+
     	return (
-    		<div className='Drawer'>Drawer</div>
+    		<div className='Drawer'>
+    			<div className='show-drawer' style={{ transform: `translateX(${ position }px)` }}>
+    				{ children }
+    			</div>
+
+    			{ isOperateShow && operateDrawer }
+    		</div>
     	);
     }
 }
--- src/modules/Drawer_demo/index.js
+++ src/modules/Drawer_demo/index.js
@@ -2,29 +2,35 @@
  * @Author: zy9@github.com/zy410419243
  * @Date: 2018-09-14 13:55:48
  * @Last Modified by: zy9
- * @Last Modified time: 2018-09-14 13:58:47
+ * @Last Modified time: 2018-09-14 14:47:01
  */
 import React, { Component } from 'react';
 
 import Drawer from '../../component/Drawer';
+
+const defaultWidth = 400;
 
 export default class DrawerDemo extends Component {
 	constructor (props) {
 		super(props);
 
 		this.state = {
-
+			position: -defaultWidth,
 		};
 	}
 
     componentDidMount = () => {
-
+    	setTimeout(() => {
+    		this.setState({ position: 0 });
+    	}, 500);
     }
 
     render = () => {
+    	const { position } = this.state;
+
     	return (
     		<div className='Drawer_demo'>
-    			<Drawer>
+    			<Drawer position={ position }>
                     test
     			</Drawer>
     		</div>
