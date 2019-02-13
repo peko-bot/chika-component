f64ec6cb9cc35d18b4364dfaf6b59f126fed7c72
zy410419243 <zy410419243@gmail.com>
2019-02-13 13:41:26 +0800

    check: update nino-cli for compile && update dekko

--- scripts/dekko-dist.js
+++ scripts/dekko-dist.js
@@ -4,8 +4,16 @@ const chalk = require('chalk');
 try {
   $('dist/lib')
     .isDirectory()
-    .hasFile('canvas.js')
-    .hasFile('toolbar.js');
+    .hasDirectory('Button')
+    .hasDirectory('Calendar')
+    .hasDirectory('Container')
+    .hasDirectory('Drawer')
+    .hasDirectory('EasyLeaflet')
+    .hasDirectory('Progress')
+    .hasDirectory('Ripple')
+    .hasDirectory('Swiper')
+    .hasDirectory('Tabs')
+    .hasDirectory('Upload');
 } catch (error) {
   // eslint-disable-next-line
   console.log(chalk.red('✨ ' + error));
--- scripts/dekko-lib.js
+++ scripts/dekko-lib.js
@@ -5,11 +5,21 @@ try {
   $('lib')
     .isDirectory()
     .hasFile('index.js')
-    .hasDirectory('canvas')
-    .hasDirectory('line')
-    .hasDirectory('options')
-    .hasDirectory('tools')
-    .hasDirectory('utils');
+    .hasDirectory('assets')
+    .hasDirectory('component')
+    .hasDirectory('util');
+  $('lib/component')
+    .isDirectory()
+    .hasDirectory('Button')
+    .hasDirectory('Calendar')
+    .hasDirectory('Container')
+    .hasDirectory('Drawer')
+    .hasDirectory('EasyLeaflet')
+    .hasDirectory('Progress')
+    .hasDirectory('Ripple')
+    .hasDirectory('Swiper')
+    .hasDirectory('Tabs')
+    .hasDirectory('Upload');
 } catch (error) {
   // eslint-disable-next-line
   console.log(chalk.red('✨ ' + error));
