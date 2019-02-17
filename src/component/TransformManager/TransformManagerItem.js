8696771cae5956a7643a6fe298196fd69ba6bb58
zy410419243 <zy410419243@gmail.com>
2019-02-17 22:41:12 +0800

    feat(TransformManager): collect props from Item & handle and render them

---
+++ src/component/TransformManager/TransformManagerItem.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+
+export default class TransformManagerItem extends Component {
+  static propTypes = {
+    group: PropTypes.string,
+    order: PropTypes.number,
+    className: PropTypes.string,
+    children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]),
+  };
+
+  static defaultProps = {
+    group: '',
+    order: 0,
+    className: '',
+    children: null,
+  };
+
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  componentDidMount = () => {};
+
+  render = () => {
+    const { className, children, ...rest } = this.props;
+    return (
+      <div className={classNames('TransformManagerItem', className)} {...rest}>
+        {children}
+      </div>
+    );
+  };
+}
