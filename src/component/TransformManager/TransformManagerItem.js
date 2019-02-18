8418e37d146d67227775836926f4bc231d839f44
zy410419243 <zy410419243@gmail.com>
2019-02-18 13:50:40 +0800

    chore(TransformManager): Component => PureComponent

--- src/component/TransformManager/TransformManagerItem.js
+++ src/component/TransformManager/TransformManagerItem.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 
-export default class TransformManagerItem extends Component {
+export default class TransformManagerItem extends PureComponent {
   static propTypes = {
     group: PropTypes.string,
     order: PropTypes.number,
