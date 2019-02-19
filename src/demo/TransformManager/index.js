f1799c75f588d2f97464180e4b1b30db661e7041
zy410419243 <zy410419243@gmail.com>
2019-02-19 09:58:02 +0800

    pref(TransformManager): set position by order

--- src/demo/TransformManager/index.js
+++ src/demo/TransformManager/index.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react';
 import TransformManager, { Item } from '../../component/TransformManager';
+import { Button } from 'antd-mobile';
 
 export default class TransformManagerDemo extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      currentOrder: 0,
+    };
+  }
+  handleCurrentPosition = () => {
+    let { currentOrder } = this.state;
+    this.setState({ currentOrder: ++currentOrder });
+  };
+
   render = () => {
+    const { currentOrder } = this.state;
     return (
       <div className="TransformManagerDemo">
-        <TransformManager currentGroup="group1" currentOrder={2}>
+        <Button type="primary" onClick={this.handleCurrentPosition}>
+          click
+        </Button>
+        <TransformManager currentGroup="group1" currentOrder={currentOrder}>
           <Item group="group1" order={0} key="group1-0">
             group1-0
           </Item>
