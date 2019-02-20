7fafe89c8bfe40609347003561ae3a3813ce8dff
zy410419243 <zy410419243@gmail.com>
2019-02-20 11:17:06 +0800

    feat: render another group

--- src/component/TransformManager/TransformManager.js
+++ src/component/TransformManager/TransformManager.js
@@ -3,6 +3,9 @@ import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import './css/TransformManager.css';
 
+let groupTypes = [];
+let groups = [];
+
 const getGroupTypes = childProps => {
   let result = [];
   for (let item of childProps) {
@@ -43,21 +46,38 @@ export default class TransformManager extends PureComponent {
     React.Children.map(nextProps.children, child => {
       childProps.push(Object.assign({}, child.props, { key: child.key }));
     });
-    const groupTypes = getGroupTypes(childProps);
-    const groups = generateGroups(groupTypes, childProps);
+    groupTypes = getGroupTypes(childProps);
+    groups = generateGroups(groupTypes, childProps);
 
     if (nextState.currentGroup === '') {
       nextState.currentGroup = nextProps.currentGroup;
     }
     if (nextProps.currentGroup === nextState.currentGroup) {
       nextState.display = groups[nextProps.currentGroup];
+      nextState.currentOrder = nextProps.currentOrder;
       return nextState;
     }
+    // when group name is different, animation of next group item
+    // will disappear. The effect of the following code is to solve this.
+    //
+    // 1. find the index in state where order is 0,
+    //    and get subarray from state that slice with the index.
+    // 2. add target group item from props in the subarray
+    // 3. return display
+    // 4. update state in componentDidUpdate
     if (nextProps.currentGroup !== nextState.currentGroup) {
-      nextState.display = [
-        ...groups[nextProps.currentGroup],
-        ...groups[nextState.currentGroup],
-      ];
+      const prevGroupIndex = groups[nextState.currentGroup].findIndex(
+        current => current.order === 0,
+      );
+      const prevGroup = groups[nextState.currentGroup].slice(
+        0,
+        prevGroupIndex + 1,
+      );
+      const nextDisplayItem =
+        groups[nextProps.currentGroup][nextProps.currentOrder];
+      nextDisplayItem.order = prevGroup.length;
+      nextState.display = [...prevGroup, nextDisplayItem];
+      nextState.currentOrder = 1;
       nextState.currentGroup = nextProps.currentGroup;
       return nextState;
     }
@@ -68,24 +88,31 @@ export default class TransformManager extends PureComponent {
 
     this.state = {
       currentGroup: '',
+      currentOrder: 0,
       display: [],
+      history: [],
     };
   }
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevProps.currentGroup !== prevState.currentGroup) {
+      this.setState({ display: groups[prevState.currentGroup] });
+    }
+  };
 
   renderChildren = () => {
     if (Object.keys(this.state).length < 2) {
       return null;
     }
-    const { props, state } = this;
+    const { display, currentOrder } = this.state;
     let result = [];
-    for (let item of state.display) {
+    for (let item of display) {
       const { children, order, style, className, ...rest } = item;
       result.push(
         <div
           style={Object.assign(
             {
-              transform: `translate3d(${(order - props.currentOrder) *
-                100}%, 0, 0)`,
+              transform: `translate3d(${(order - currentOrder) * 100}%, 0, 0)`,
             },
             style,
           )}
