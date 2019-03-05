d602a6ddf3182348948d5b3fa3df45adabd11a85
zy410419243 <zy410419243@gmail.com>
2019-03-05 19:54:05 +0800

    pref: remove Button from dekko

--- scripts/dekko-lib.js
+++ scripts/dekko-lib.js
@@ -1,27 +1,22 @@
 const $ = require('dekko');
 const chalk = require('chalk');
 
-try {
-  $('lib')
-    .isDirectory()
-    .hasFile('index.js')
-    .hasDirectory('assets')
-    .hasDirectory('component')
-    .hasDirectory('util');
-  $('lib/component')
-    .isDirectory()
-    .hasDirectory('Button')
-    .hasDirectory('Calendar')
-    .hasDirectory('Container')
-    .hasDirectory('Drawer')
-    .hasDirectory('EasyLeaflet')
-    .hasDirectory('Progress')
-    .hasDirectory('Ripple')
-    .hasDirectory('Swiper')
-    .hasDirectory('Tabs')
-    .hasDirectory('Upload');
-} catch (error) {
-  // eslint-disable-next-line
-  console.log(chalk.red('✨ ' + error));
-  throw Error(error);
-}
+$('lib')
+  .isDirectory()
+  .hasFile('index.js')
+  .hasDirectory('assets')
+  .hasDirectory('component')
+  .hasDirectory('util');
+$('lib/component')
+  .isDirectory()
+  .hasDirectory('Calendar')
+  .hasDirectory('Container')
+  .hasDirectory('Drawer')
+  .hasDirectory('EasyLeaflet')
+  .hasDirectory('Progress')
+  .hasDirectory('Ripple')
+  .hasDirectory('Swiper')
+  .hasDirectory('Tabs')
+  .hasDirectory('Upload');
+// eslint-disable-next-line
+console.log(chalk.red('✨ ' + error));
