5122492afa1ae9bbbc2085f325867587cd638081
zy410419243 <zy410419243@gmail.com>
2019-03-17 16:32:29 +0800

    chore: remove related configs of EasyLeaflet

--- scripts/nino.koei.js
+++ scripts/nino.koei.js
@@ -9,7 +9,6 @@ module.exports = {
     'lib/Calendar/index': path.join(cwd + '/src/component/Calendar'),
     'lib/Container/index': path.join(cwd + '/src/component/Container'),
     'lib/Drawer/index': path.join(cwd + '/src/component/Drawer'),
-    'lib/EasyLeaflet/index': path.join(cwd + '/src/component/EasyLeaflet'),
     'lib/Progress/index': path.join(cwd + '/src/component/Progress'),
     'lib/Ripple/index': path.join(cwd + '/src/component/Ripple'),
     'lib/Swiper/index': path.join(cwd + '/src/component/Swiper'),
