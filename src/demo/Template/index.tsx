4cf85874ef078a9ed028f076fc7c3304dfc58c9e
zy410419243 <zy410419243@gmail.com>
2019-03-22 21:48:50 +0800

    chore: update demo

--- src/demo/Template/index.tsx
+++ src/demo/Template/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Template from '../../component/Template';
 
+const dataSource = [
+  { test1: 'test1', test2: 'test2', id: 'template-1' },
+  { test1: 'test11', test2: 'test22', id: 'template-2' },
+];
+
 const TempLateDemo = () => (
-  <Template>
-    <div>test1</div>
-    <div>test2</div>
+  <Template dataSource={dataSource}>
+    <>
+      <div data-key="test1" />
+      <div data-key="test2" />
+    </>
   </Template>
 );
 
