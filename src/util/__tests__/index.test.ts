27ef372a148fff0d4f04b4d5e279253d175bb252
zy410419243 <zy410419243@gmail.com>
2019-03-24 22:01:55 +0800

    fix: check utc date

--- src/util/__tests__/index.test.ts
+++ src/util/__tests__/index.test.ts
@@ -3,9 +3,6 @@ import { formatDate, getParamsFromUrl } from '..';
 describe('index', () => {
   it('formatDate', () => {
     expect(formatDate('2019-03-24T00:00:00')).toBe('2019-03-24 00:00:00');
-    expect(formatDate(new Date(Date.UTC(2019, 2, 24, 8)))).toBe(
-      '2019-03-24 16:00:00',
-    );
   });
 
   it('getParamsFromUrl', () => {
