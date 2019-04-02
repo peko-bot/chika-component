0615bc751d6a11f096a3972e99e815f278fe6dc1
orzorzorzorz <zy410419243@gmail.com>
2019-04-02 15:53:40 +0800

    Reactor: Swiper (#58)
    * chore: redesign api
    
    * refactor: remove useless line
    
    * refactor: merge refresh and load into spinner
    
    * refacor: clean up
    
    * refactor: replace cancel with using prop loading, not function
    
    * test: update snapshots

--- src/component/Swiper/index.tsx
+++ src/component/Swiper/index.tsx
@@ -1,104 +1,135 @@
 import React from 'react';
 import './css/Swiper.css';
+import classNames from 'classnames';
 
+type Status = 'loading' | 'loaded';
+type SpinnerType = 'load' | 'refresh';
+type SpinnerParam = {
+  type: SpinnerType;
+  relatedStyle: React.CSSProperties;
+  relatedText: string;
+  iconUrl: string;
+  iconDeg: number;
+  isFinished: boolean;
+};
 export interface SwiperProps {
-  config: {
-    wrapperHeight: number;
-    sensibility: number;
-    duration: number;
-    refresh?: any;
-    load?: any;
-  };
-  refresh?: () => void;
-  load?: () => void;
+  wrapperHeight: number;
+  loading: boolean;
+  sensibility?: number;
+  duration?: number;
+  onRefresh?: () => void;
+  onLoad?: () => void;
 }
 export interface SwiperState {
   distance: number;
   iconDeg: number;
   refreshEnd: boolean;
   loadEnd: boolean;
-  duration: number | string;
   refreshText: string;
   refreshImg: string;
   loadText: string;
   loadImg: string;
+  status: Status;
+  type: SpinnerType;
 }
+const down = '../../assets/Swiper/down.png';
+const loading = '../../assets/Swiper/loading.gif';
+const complete = '../../assets/Swiper/complete.png';
 
-/* 基于translate3d的滑动分页组件
-    尚未实现惯性缓动
-*/
 export default class Swiper extends React.Component<SwiperProps, SwiperState> {
+  wrapper: HTMLDivElement | null;
+  scroller: HTMLDivElement;
   startY = 0;
   endY = 0;
-  down = '../../assets/Swiper/down.png';
-  loading = '../../assets/Swiper/loading.gif';
-  complete = '../../assets/Swiper/complete.png';
-  scrollerHeight = 1000;
+  scrollerHeight = 10000;
   bottomHeight = 0;
-  wrapper: any;
-  scroller: any;
-  timer: any;
 
-  constructor(props: SwiperProps) {
-    super(props);
-    this.state = {
-      distance: 0, // 从touchstart记，到touchend结束，容器滑过的距离
-      iconDeg: 0, // 箭头变换角度
-      refreshEnd: false, // 刷新是否完成
-      loadEnd: false, // 加载是否完成
-      duration: 0, // 滑动持续时间，如果config中传了这个参数则默认失效
-      refreshText: '下拉刷新',
-      refreshImg: this.down,
-      loadText: '加载更多',
-      loadImg: this.down,
-    };
+  state: SwiperState = {
+    distance: 0, // 从touchstart记，到touchend结束，容器滑过的距离
+    iconDeg: 0, // 箭头变换角度
+    refreshEnd: false, // 刷新是否完成
+    loadEnd: false, // 加载是否完成
+    refreshText: '下拉刷新',
+    refreshImg: down,
+    loadText: '加载更多',
+    loadImg: down,
+    status: 'loaded',
+    type: 'load',
+  };
+
+  componentDidUpdate(prevProps: SwiperProps, prevState: SwiperState) {
+    if (
+      prevProps.loading &&
+      !this.props.loading &&
+      prevState.status === 'loading'
+    ) {
+      if (this.state.type === 'refresh') {
+        this.setState(
+          {
+            status: 'loaded',
+            refreshImg: complete,
+            refreshText: '刷新成功',
+            iconDeg: 0,
+          },
+          () => {
+            setTimeout(() => {
+              this.getChildHeight();
+              this.setState({
+                refreshImg: down,
+                distance: 0,
+                refreshText: '下拉刷新',
+              });
+            }, 1000);
+          },
+        );
+      } else if (this.state.type === 'load') {
+        this.setState(
+          {
+            status: 'loaded',
+            loadImg: complete,
+            loadText: '加载完成',
+            iconDeg: 0,
+          },
+          () => {
+            setTimeout(() => {
+              this.getChildHeight();
+              this.setState({
+                loadImg: down,
+                loadText: '加载更多',
+                distance: this.endY,
+              });
+            }, 1000);
+          },
+        );
+      }
+    }
   }
 
   componentDidMount = () => {
-    this.eventBind(this.wrapper);
+    this.bindEvent(this.wrapper);
   };
 
-  reset = () => {
-    this.getChildHeight();
-  };
-
-  /* 获得childeren元素加载的高度
-        这里算法是用 屏幕可视高度-传进来容器高度,
-        得到底部div该放到什么地方 */
   getChildHeight = () => {
-    const { wrapperHeight } = this.props.config;
+    const { wrapperHeight } = this.props;
     const { scrollHeight } = this.scroller;
-    // let height = document.documentElement.clientHeight || document.body.clientHeight;
-    // 父页面中非容器元素的高度
-    // this.n_differHeight = height - wrapperHeight;
-    // 触发加载时要停在底部显示加载中
-
     this.bottomHeight = scrollHeight - wrapperHeight + 44;
     this.scrollerHeight = scrollHeight;
-
-    // this.setState({});
   };
 
-  // 上下两div行为相似，于是放到一块去绑定事件了
-  eventBind = (refs: any) => {
-    const { config } = this.props;
+  bindEvent = (refs: any) => {
+    const { sensibility = 1 } = this.props;
 
     refs.addEventListener('touchstart', (e: any) => {
-      // 计时
-      this.timer = new Date().getTime();
-      // 记录开始滑动时的坐标
+      // record coordinate before touch start
       this.startY = e.touches[0].pageY;
-
       this.setState({ refreshEnd: false, loadEnd: false });
     });
 
     refs.addEventListener('touchmove', (e: any) => {
-      // 放在touchstart里会覆盖children里的click事件
       e.preventDefault();
-
-      // 计算容器滑过的距离
-      let distance =
-        (e.touches[0].pageY - this.startY) / config.sensibility + this.endY;
+      // distance of wrapper moving
+      const distance =
+        (e.touches[0].pageY - this.startY) / sensibility + this.endY;
       let iconDeg;
       if (distance > 44) {
         // 44是刷新、加载div的高度，超过时箭头向上
@@ -110,30 +141,23 @@ export default class Swiper extends React.Component<SwiperProps, SwiperState> {
         // 下方的加载偷懒没写箭头变换以及未超过44的行为，mark
         iconDeg = 180;
       }
-
       this.setState({ distance, iconDeg });
     });
 
     refs.addEventListener('touchend', () => {
-      /*
-        配置里有duration的话就用配置里的
-        默认的duration是从touchstart到end的时间
-        也就是说滑动时间越长，弹回的越慢
-      */
-      let duration = config.duration
-        ? config.duration
-        : (new Date().getTime() - this.timer) / 1000;
-      let { distance } = this.state;
-
+      const { onRefresh, onLoad } = this.props;
+      const { distance } = this.state;
       if (distance > 44) {
         // 拖到顶部的情况
-        if (this.props.refresh) {
+        if (onRefresh) {
           this.setState({
-            refreshImg: this.loading,
+            refreshImg: loading,
             refreshText: '刷新中...',
             distance: 44,
+            status: 'loading',
+            type: 'refresh',
           });
-          this.props.refresh();
+          onRefresh();
         }
       } else if (distance > 0 && distance <= 44) {
         // 上方div未超过44的情况
@@ -142,19 +166,20 @@ export default class Swiper extends React.Component<SwiperProps, SwiperState> {
         // 拖动到底部的情况
         const { offsetHeight, scrollHeight } = this.scroller;
         const differHeight = scrollHeight - offsetHeight;
-
         // 超出边界回弹
+        // 这里不能写等于，不然容器拖到底部时，touchstart会触发加载事件
         if (Math.abs(distance) > differHeight) {
-          // 这里不能写等于，不然容器拖到底部时，touchstart会触发加载事件
           this.endY = -differHeight;
           // 下拉加载
-          if (this.props.load) {
-            this.props.load();
+          if (onLoad) {
             this.setState({
               loadText: '加载中...',
-              loadImg: this.loading,
+              loadImg: loading,
               distance: -this.bottomHeight,
+              status: 'loading',
+              type: 'load',
             });
+            onLoad();
           } else {
             this.setState({ distance: -differHeight });
           }
@@ -164,169 +189,103 @@ export default class Swiper extends React.Component<SwiperProps, SwiperState> {
         }
       }
 
-      this.setState(
-        {
-          refreshEnd: true,
-          loadEnd: true,
-          duration,
-        },
-        () => {
-          this.timer = null;
-        },
-      );
+      this.setState({
+        refreshEnd: true,
+        loadEnd: true,
+      });
     });
   };
 
-  cancelRefresh = () => {
-    // 上方div在加载完后显示更新成功
-    this.setState(
-      {
-        refreshImg: this.complete,
-        refreshText: '刷新成功',
-        iconDeg: 0,
-      },
-      () => {
-        // 滑回去
-        setTimeout(() => {
-          this.reset();
-          this.setState({
-            refreshImg: this.down,
-            distance: 0,
-            refreshText: '下拉刷新',
-          });
-        }, 1000);
-      },
-    );
+  generateStyle = () => {
+    const { wrapperHeight, duration = 1 } = this.props;
+    const { distance, refreshEnd, loadEnd } = this.state;
+    const clientHeight =
+      document.documentElement.clientHeight || document.body.clientHeight;
+    const refreshStyle: React.CSSProperties = {};
+    const loadStyle: React.CSSProperties = {};
+    let wrapperStyle: React.CSSProperties = {};
+    const transform = `translate3d(0px, ${distance}px, 0)`;
+    const transition = `all ${duration}s ease`;
+
+    refreshStyle.transform = transform;
+    if (refreshEnd) refreshStyle.transition = transition;
+
+    loadStyle.transform = transform;
+    loadStyle.top = this.scrollerHeight;
+    if (loadEnd) loadStyle.transition = transition;
+
+    wrapperStyle = {
+      ...refreshStyle,
+      height: wrapperHeight || clientHeight,
+    };
+
+    return { wrapperStyle, loadStyle, refreshStyle };
   };
 
-  cancelLoad = (finishText = '加载完成') => {
-    // 下方div在加载完后显示加载成功
-    this.setState(
-      {
-        loadImg: this.complete,
-        loadText: finishText,
-        iconDeg: 0,
-      },
-      () => {
-        // 滑回去
-        setTimeout(() => {
-          this.reset();
-          this.setState({
-            loadImg: this.down,
-            distance: this.endY,
-            loadText: '加载更多',
-          });
-        }, 1000);
-      },
+  renderSpinner = ({
+    type,
+    isFinished,
+    relatedStyle,
+    iconUrl,
+    iconDeg,
+    relatedText,
+  }: SpinnerParam) => {
+    return (
+      <div
+        className={classNames({
+          [type]: true,
+          [`${type}-end`]: isFinished,
+        })}
+        style={relatedStyle}
+      >
+        <span
+          className={`${type}-icon`}
+          style={{ transform: `rotateZ(${iconDeg}deg)` }}
+        >
+          <img src={iconUrl} />
+        </span>
+        <span className={`${type}-text`}>{relatedText}</span>
+      </div>
     );
   };
 
   render() {
-    const { config } = this.props,
-      { wrapperHeight } = config;
+    const { onRefresh, onLoad, children } = this.props;
     const {
-      distance,
       iconDeg,
       refreshEnd,
-      duration,
       refreshText,
       refreshImg,
       loadEnd,
       loadText,
       loadImg,
     } = this.state;
-    const defaultHeight =
-      document.documentElement.clientHeight || document.body.clientHeight;
-
-    let refreshStyle = {
-      transform: `translate3d(0px, ${distance}px, 0)`,
-    };
-
-    if (refreshEnd)
-      refreshStyle = Object.assign(refreshStyle, {
-        transition: `all ${duration}s ease`,
-      });
-
-    let loadStyle = {
-      transform: `translate3d(0px, ${distance}px, 0)`,
-      top: this.scrollerHeight,
-    };
-
-    if (loadEnd)
-      loadStyle = Object.assign(loadStyle, {
-        top: this.scrollerHeight,
-        transition: `all ${duration}s ease`,
-      });
-
-    let wrapStyle = Object.assign(
-      {},
-      { height: wrapperHeight || defaultHeight },
-      refreshStyle,
-    );
-
-    let refresh: Array<any> = [];
-
-    if (config.refresh === true || config.refresh === undefined) {
-      refresh.push(
-        <div
-          key="refresh_wrapper"
-          className={refreshEnd ? 'refresh refresh-end' : 'refresh'}
-          style={refreshStyle}
-        >
-          <span
-            className="refresh-icon"
-            style={{ transform: `rotateZ(${iconDeg}deg)` }}
-          >
-            <img src={refreshImg} />
-          </span>
-          <span className="refresh-text">{refreshText}</span>
-        </div>,
-      );
-    }
-
-    let load: Array<any> = [];
-
-    if (config.load === true || config.load === undefined) {
-      load.push(
-        <div
-          key="load_wrapper"
-          className={loadEnd ? 'load load-end' : 'load'}
-          style={loadStyle}
-        >
-          <span
-            className="load-icon"
-            style={{ transform: `rotateZ(${iconDeg}deg)` }}
-          >
-            <img src={loadImg} />
-          </span>
-          <span className="load-text">{loadText}</span>
-        </div>,
-      );
-    }
+    const { refreshStyle, wrapperStyle, loadStyle } = this.generateStyle();
 
     return (
       <div className="Swiper">
-        <div ref={(ref: any) => (this.wrapper = ref)} className="wrapper">
-          {React.Children.map(this.props.children, (child: any) => {
-            child = React.cloneElement(child, { className: 'child-z-index-8' });
-
-            return (
-              <div>
-                <div
-                  style={wrapStyle}
-                  ref={(ref: any) => (this.scroller = ref)}
-                >
-                  {child}
-                </div>
-
-                {/* 上拉刷新 */}
-                {refresh}
-
-                {/* 下拉加载 */}
-                {load}
-              </div>
-            );
-          })}
+        <div ref={ref => (this.wrapper = ref)} className="wrapper">
+          <div style={wrapperStyle} ref={ref => ref && (this.scroller = ref)}>
+            {children}
+          </div>
+          {onRefresh &&
+            this.renderSpinner({
+              type: 'refresh',
+              isFinished: refreshEnd,
+              relatedStyle: refreshStyle,
+              iconUrl: refreshImg,
+              iconDeg,
+              relatedText: refreshText,
+            })}
+          {onLoad &&
+            this.renderSpinner({
+              type: 'load',
+              isFinished: loadEnd,
+              relatedStyle: loadStyle,
+              iconUrl: loadImg,
+              iconDeg,
+              relatedText: loadText,
+            })}
         </div>
       </div>
     );
--- src/demo/Swiper/index.tsx
+++ src/demo/Swiper/index.tsx
@@ -1,80 +1,52 @@
 import React from 'react';
 import Swiper from '../../component/Swiper';
 import './css/Swiper_demo.css';
-const secRecordPath = '/mock/s_problem_record_hy.json';
+const secRecordPath = '/mock/swiper.json';
+import { ajax } from '../../util/urlHelper';
 
 export default class SwiperDemo extends React.Component {
   swiper: any;
-  state: { datas: Array<any> } = {
+  state: { datas: Array<any>; loading: boolean } = {
     datas: [],
+    loading: false,
   };
 
   dataSource = [];
   componentDidMount = () => {
-    fetch(secRecordPath)
-      .then(result => result.json())
-      .then(result => {
-        this.setState({ datas: result }, () => {
-          this.swiper.reset();
-        });
-      });
+    this.setState({ loading: true });
+    ajax({
+      url: secRecordPath,
+      success: result => this.setState({ datas: result, loading: false }),
+    });
   };
 
-  refresh = () => {
-    fetch(secRecordPath)
-      .then(result => result.json())
-      .then(result => {
-        setTimeout(() => {
-          this.setState({ datas: result }, () => {
-            this.swiper.cancelRefresh();
-          });
-        }, 1000);
-      });
-  };
-
-  load = () => {
+  onChange = () => {
+    this.setState({ loading: true });
     setTimeout(() => {
-      this.swiper.cancelLoad();
+      this.setState({ loading: false });
     }, 1000);
   };
 
-  onClick = (i: number) => {
-    // eslint-disable-next-line
-    console.log(i);
-  };
-
   render() {
-    const { onClick } = this;
+    const { loading } = this.state;
 
     return (
       <div className="Swiper_demo">
         <Swiper
-          config={this.config}
-          refresh={this.refresh}
-          load={this.load}
-          ref={ref => (this.swiper = ref)}
+          wrapperHeight={500}
+          duration={0.7}
+          sensibility={1}
+          onRefresh={this.onChange}
+          onLoad={this.onChange}
+          loading={loading}
         >
           <ul>
             {this.state.datas.map((item, i) => {
-              return (
-                <li key={`datas_${i}`} onClick={() => onClick(item)}>
-                  {item}
-                </li>
-              );
+              return <li key={i}>{item}</li>;
             })}
           </ul>
         </Swiper>
       </div>
     );
   }
-
-  height = document.documentElement.clientHeight || document.body.clientHeight;
-
-  config = {
-    wrapperHeight: 500, // 容器高度
-    duration: 0.5, // 弹回时间
-    sensibility: 1, // 灵敏度
-    // refresh: false,
-    // load: false,
-  };
 }
