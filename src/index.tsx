77556d9419f69aa2ffe8d41d01f4157a6aab6ad5
zy410419243 <zy410419243@gmail.com>
2019-04-03 09:38:22 +0800

    chore: add missing export of MapBox

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 export { default as Calendar } from './component/Calendar';
 
+export { default as Container } from './component/Container';
+
 export { default as Drawer } from './component/Drawer';
 
-export { default as Container } from './component/Container';
+export { default as MapBox } from './component/MapBox';
 
 export { default as Ripple } from './component/Ripple';
 
@@ -10,8 +12,8 @@ export { default as Swiper } from './component/Swiper';
 
 export { default as Tabs } from './component/Tabs';
 
-export { default as Upload } from './component/Upload';
+export { default as Template } from './component/Template';
 
 export { default as TransformManager } from './component/TransformManager';
 
-export { default as Template } from './component/Template';
+export { default as Upload } from './component/Upload';
