43132f85e9b7883f4cd355ffd3bd60f6d7d9db96
zy410419243 <zy410419243@gmail.com>
2019-04-07 10:12:45 +0800

    ci: update dist build

--- scripts/dekko-dist.js
+++ scripts/dekko-dist.js
@@ -2,24 +2,26 @@ const $ = require('dekko');
 const chalk = require('chalk');
 
 try {
-  $('lib')
+  $('dist')
     .isDirectory()
-    .hasFile('index.js')
     .hasDirectory('assets')
-    .hasDirectory('component')
-    .hasDirectory('utils');
-  $('lib/component')
+    .hasDirectory('lib')
+    .hasDirectory('mock')
+    .hasFile('index.html')
+    .hasFile('ninoninoni.js')
+    .hasFile('ninoninoni.js.map');
+  $('dist/lib')
     .isDirectory()
     .hasDirectory('Calendar')
     .hasDirectory('Container')
     .hasDirectory('Drawer')
+    .hasDirectory('MapBox')
     .hasDirectory('Ripple')
     .hasDirectory('Swiper')
     .hasDirectory('Tabs')
-    .hasDirectory('Upload')
     .hasDirectory('Template')
-    .hasDirectory('MapBox')
-    .hasDirectory('Template');
+    .hasDirectory('TransformManager')
+    .hasDirectory('Upload');
 } catch (error) {
   // eslint-disable-next-line
   console.log(chalk.red('✨ ' + error));
