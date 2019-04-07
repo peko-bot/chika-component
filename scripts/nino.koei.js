18957a7e0ecea5b4b3ab7b3ee27338f71fbc38f3
orzorzorzorz <zy410419243@gmail.com>
2019-04-07 09:57:43 +0800

    chore: update entry of build (#72)
    * chore: update entry of build
    
    * chore: travel src/component for build

--- scripts/nino.koei.js
+++ scripts/nino.koei.js
@@ -1,31 +1,21 @@
 const path = require('path');
+const fs = require('fs-extra');
 const cwd = process.cwd();
+
+const fileList = fs.readdirSync(path.join(cwd, 'src/component/'));
+const entry = {};
+for (let item of fileList) {
+  entry['lib/' + item + '/index'] = path.join(cwd, 'src/component/' + item);
+}
+entry['ninoninoni'] = path.join(cwd, '/src/go');
 
 module.exports = {
   mode: 'development',
   devtool: 'source-map',
-  entry: {
-    'lib/Calendar/index': path.join(cwd + '/src/component/Calendar'),
-    'lib/Container/index': path.join(cwd + '/src/component/Container'),
-    'lib/Drawer/index': path.join(cwd + '/src/component/Drawer'),
-    'lib/Progress/index': path.join(cwd + '/src/component/Progress'),
-    'lib/Ripple/index': path.join(cwd + '/src/component/Ripple'),
-    'lib/Swiper/index': path.join(cwd + '/src/component/Swiper'),
-    'lib/Tabs/index': path.join(cwd + '/src/component/Tabs'),
-    'lib/TransformManager/index': path.join(
-      cwd + '/src/component/TransformManager',
-    ),
-    'lib/Upload/index': path.join(cwd + '/src/component/Upload'),
-    'lib/MapBox/index': path.join(cwd + '/src/component/MapBox'),
-    'lib/Template/index': path.join(cwd + '/src/component/Template'),
-  },
+  entry,
   output: {
-    path: path.join(cwd + '/dist'),
+    path: path.join(cwd, '/dist'),
     filename: '[name].js',
     libraryTarget: 'umd',
-  },
-  externals: {
-    react: 'react',
-    'react-dom': 'react-dom',
   },
 };
