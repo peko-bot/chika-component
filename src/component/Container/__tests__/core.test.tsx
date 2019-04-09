3983b6457a84610869ac62c34923c1cb1d25f1bf
zy410419243 <zy410419243@gmail.com>
2019-04-09 16:48:13 +0800

    test: update cases for SearchBar

--- src/component/Container/__tests__/core.test.tsx
+++ src/component/Container/__tests__/core.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import 'nino-cli/scripts/setup';
 import { originConfig } from '../../../mock/config';
 import { originDataSource } from '../../../mock/dataSource';
@@ -32,5 +32,34 @@ describe('core', () => {
       </Core>,
     );
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it('onSearch should be called when SearchBar submit', () => {
+    const onSearch = jest.fn();
+    const wrapper = mount(
+      <Core
+        dataSource={originDataSource}
+        formatControls={formatControls}
+        onSearch={onSearch}
+        config={formatConfig(originConfig)}
+      >
+        <>
+          <div data-key="pjnm">1</div>
+          <div data-key="dam_cd">2</div>
+        </>
+      </Core>,
+    );
+    wrapper.setState({ showSearchBar: true });
+    wrapper.find('Button').simulate('click');
+    expect(onSearch).toHaveBeenCalledWith({
+      calendar: '',
+      checkbox: '',
+      dam_cd: '', // eslint-disable-line
+      datePicker: '',
+      mapPicker: '',
+      pjnm: '',
+      select: '',
+      upload: '',
+    });
   });
 });
