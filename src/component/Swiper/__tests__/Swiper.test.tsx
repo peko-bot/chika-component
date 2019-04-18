601249f1f965dffa3e5a61bf0fbefb50fdbc8fbb
zy410419243 <zy410419243@gmail.com>
2019-04-18 09:04:16 +0800

    test: update case

--- src/component/Swiper/__tests__/Swiper.test.tsx
+++ src/component/Swiper/__tests__/Swiper.test.tsx
@@ -15,6 +15,14 @@ switch (process.env.LIB_DIR) {
 }
 
 describe('Swiper', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   it('render correctly', () => {
     const wrapper = mount(
       <Swiper wrapperHeight={500} duration={0.7} sensibility={1}>
@@ -33,5 +41,32 @@ describe('Swiper', () => {
     wrapper.getChildHeight();
     expect(wrapper.bottomHeight).toBe(-456);
     expect(wrapper.scrollerHeight).toBe(0);
+  });
+
+  it('status loading', () => {
+    const wrapper = mount(
+      <Swiper wrapperHeight={500} duration={0.7} sensibility={1} loading>
+        test
+      </Swiper>,
+    );
+    const spy = jest.spyOn(wrapper.instance(), 'getChildHeight');
+    wrapper.setState({ status: 'loading', type: 'refresh' });
+    wrapper.setProps({ loading: false });
+    wrapper.update();
+    expect(wrapper.state('refreshText')).toBe('刷新成功');
+    jest.runAllTimers();
+    expect(wrapper.state('refreshText')).toBe('下拉刷新');
+    expect(spy).toHaveBeenCalled();
+
+    wrapper.setProps({ loading: true });
+    wrapper.setState({ status: 'loading', type: 'load' });
+    wrapper.update();
+
+    wrapper.setProps({ loading: false });
+    wrapper.update();
+    expect(wrapper.state('loadText')).toBe('加载完成');
+    jest.runAllTimers();
+    expect(wrapper.state('loadText')).toBe('加载更多');
+    expect(spy).toHaveBeenCalled();
   });
 });
