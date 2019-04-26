4ff487c72ac976e978076ab2f729e6a24773c3fb
orzorzorzorz <zy410419243@gmail.com>
2019-04-26 15:29:33 +0800

    test: update cases for Upload.Uploader (#112)
    * chore: remove unreachable code
    
    * chore: revert for dist
    
    * test: update cases for Upload.Uploader
    
    * chore: update

---
+++ src/component/Upload/__tests__/Uploader.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import 'nino-cli/scripts/setup';
+let Uploader;
+switch (process.env.LIB_DIR) {
+  case 'lib':
+    Uploader = require('../../../../lib/component/Upload/Uploader').default;
+    break;
+  default:
+    Uploader = require('../Uploader').default;
+    break;
+}
+
+describe('Uploader', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  const fileList = [
+    { id: 1, url: 'test1', error: false },
+    { id: 2, url: 'test2', error: false },
+  ];
+
+  it('triggerInputUpload', () => {
+    const wrapper = mount(<Uploader fileList={fileList} />);
+    wrapper
+      .find('.upload-item')
+      .props()
+      .onClick();
+    expect(wrapper.instance().uploadInput.current.value).toBe('');
+  });
+
+  it('onChange', done => {
+    const onChange = jest.fn();
+    const wrapper = shallow(
+      <Uploader fileList={fileList} onChange={onChange} />,
+    );
+    const blob = new Blob(['This is my firt trip to an island'], {
+      type: 'text/plain',
+    });
+    const file = new File([blob], 'test');
+    wrapper
+      .find('input')
+      .props()
+      .onChange({ target: { files: [file] } }, () => {
+        expect(onChange).toHaveBeenCalled();
+        done();
+      });
+  });
+});
