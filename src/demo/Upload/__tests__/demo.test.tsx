0a972981422bb1b4e4f6a83793f32a001af85115
orzorzorzorz <zy410419243@gmail.com>
2019-04-29 08:41:24 +0800

    test: update cases for demo of Upload (#115)

--- src/demo/Upload/__tests__/demo.test.tsx
+++ src/demo/Upload/__tests__/demo.test.tsx
@@ -8,4 +8,29 @@ describe('UploadDemo', () => {
     const wrapper = mount(<UploadDemo />);
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('check events', () => {
+    const wrapper = mount(<UploadDemo />);
+    wrapper
+      .find('Upload')
+      .props()
+      .onChange({ url: 'testurl', id: 'testid' });
+    expect(wrapper.state().fileList).toEqual([
+      { url: 'testurl', id: 'testid' },
+    ]);
+
+    wrapper
+      .find('Upload')
+      .props()
+      .onClick({ url: 'testurl', id: 'testid' });
+    expect(wrapper.state().fileList).toEqual([
+      { url: 'testurl', id: 'testid', error: true },
+    ]);
+
+    wrapper
+      .find('Upload')
+      .props()
+      .onPress({ url: 'testurl', id: 'testid' });
+    expect(wrapper.state().fileList).toEqual([]);
+  });
 });
