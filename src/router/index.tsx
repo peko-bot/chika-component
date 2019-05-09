8167ce33cbacc911b5b981cda8fc595a70af9ca0
orzorzorzorz <zy410419243@gmail.com>
2019-05-09 10:33:36 +0800

    pref: dynamic import modules (#126)
    * pref: dynamic import modules
    
    * test: update snapshots

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 
-import Container from '../demo/Container';
-import Swiper from '../demo/Swiper';
-import Upload from '../demo/Upload';
-import TransformManager from '../demo/TransformManager';
-import MapBox from '../demo/MapBox';
-import Template from '../demo/Template';
+const Container = lazy(() => import('../demo/Container'));
+const Swiper = lazy(() => import('../demo/Swiper'));
+const Upload = lazy(() => import('../demo/Upload'));
+const TransformManager = lazy(() => import('../demo/TransformManager'));
+const MapBox = lazy(() => import('../demo/MapBox'));
+const Template = lazy(() => import('../demo/Template'));
 
 export default () => (
   <HashRouter>
-    <Switch>
-      <Route exact path="/" component={Container} />
-      <Route path="/container" component={Container} />
-      <Route path="/swiper" component={Swiper} />
-      <Route path="/upload" component={Upload} />
-      <Route path="/transform" component={TransformManager} />
-      <Route path="/mapbox" component={MapBox} />
-      <Route path="/template" component={Template} />
-    </Switch>
+    <Suspense fallback={<div>Loading...</div>}>
+      <Switch>
+        <Route exact path="/" component={Container} />
+        <Route path="/container" component={Container} />
+        <Route path="/swiper" component={Swiper} />
+        <Route path="/upload" component={Upload} />
+        <Route path="/transform" component={TransformManager} />
+        <Route path="/mapbox" component={MapBox} />
+        <Route path="/template" component={Template} />
+      </Switch>
+    </Suspense>
   </HashRouter>
 );
